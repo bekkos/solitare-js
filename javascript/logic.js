628d6146a1965a00866b5d061cff07822558841a
bekkos <bekkosm@gmail.com>
2021-08-24 17:53:23 +0200

    Further progress. 24.08

--- javascript/logic.js
+++ javascript/logic.js
@@ -22,6 +22,10 @@ const drop = (e) => {
     e.target.appendChild(document.getElementById(data));
     moveCard(data, e.target.closest(".stack").id);
     drawCards();
+    console.log(data);
+    console.log(e.target.closest(".stack").id);
+    
+    return;
 }
 
 // ARRAYS
@@ -41,10 +45,7 @@ let deck = [
     'acecl', 'acedi', 'acehe', 'acesp'
 ];
 
-let topDeckLeftBoxOne = [];
-let topDeckLeftBoxTwo = [];
-let topDeckLeftBoxThree = [];
-let topDeckLeftBoxFour = [];
+
 
 let topDeckRightBoxOne = [];
 
@@ -55,8 +56,23 @@ let stackFour = []; // 4
 let stackFive = []; // 5
 let stackSix = []; // 6
 let stackSeven = []; // 7
-let stacks = [stackOne, stackTwo, stackThree, stackFour, stackFive, stackSix, stackSeven];
-let discovered = [0,0,0,0,0,0,0];
+
+let completionStackOne = []; // Completion Stack 1
+let completionStackTwo = []; // Completion Stack 2
+let completionStackThree = []; // Completion Stack 3
+let completionStackFour = []; // Completion Stakc 4
+
+let stacks = [stackOne, stackTwo, stackThree,
+    stackFour, stackFive, stackSix, stackSeven,
+    completionStackOne, completionStackTwo,
+    completionStackThree, completionStackFour];
+
+
+let discovered = ['2cl'];
+
+
+
+
 // Toolbox
 
 const locateCard = (id) => {
@@ -67,6 +83,7 @@ const locateCard = (id) => {
             'array': deck,
             'index': i
         }
+        console.log("lc-1");
         return data;
     }
 
@@ -77,16 +94,16 @@ const locateCard = (id) => {
                 'array': stacks[i],
                 'index': x
             }
+            console.log("lc-2: " + data.array + " " + data.index);
             return data;
         }
     }
-
+    console.log("lc-3");
     return -1;
 }
 
 const moveCard = (id, destination) => {
     let location = locateCard(id);
-    
     if (location == -1) return;
 
     try {
@@ -95,7 +112,7 @@ const moveCard = (id, destination) => {
 
     location.array.splice(location.index, 1);
     eval(destination).push(id);
-    
+
     if (children != null) {
         for (let i = 0; i < children.length; i++) {
             location.array.splice(location.index, 1);
@@ -159,16 +176,13 @@ const initiateGame = () => {
         stackSeven.push(deck[index]);
         deck.splice(index, 1);
     }
-
-
     drawCards();
-
 }
 
 const drawCards = () => {
     let elementId;
     for (let z = 0; z < stacks.length; z++) {
-        if(stacks[z].length < 1) continue;
+        if (stacks[z].length < 1) continue;
 
         switch (z) {
             case 0:
@@ -192,13 +206,25 @@ const drawCards = () => {
             case 6:
                 elementId = "stackSeven";
                 break;
+            case 7:
+                elementId = "completionStackOne";
+                break;
+            case 8:
+                elementId = "completionStackTwo";
+                break;
+            case 9:
+                elementId = "completionStackThree";
+                break;
+            case 10:
+                elementId = "completionStackFour";
+                break;
             default:
                 break;
         }
 
 
         render = document.getElementById(elementId);
-        if(stacks[z].length <= 1) {
+        if (stacks[z].length <= 1) {
             render.innerHTML = `
             <div id="${stacks[z][0]}" class="card" draggable="true" ondragstart="drag(event)">
                 <img src="./media/cards/${stacks[z][0]}.png" draggable="false"   alt="">
@@ -211,30 +237,39 @@ const drawCards = () => {
                 </div>
                 `;
         }
-        
+
 
         let stackLastNote = document.getElementById(stacks[z][0]);
         for (let y = 1; y < stacks[z].length; y++) {
-            if(y == stacks[z].length-1) {
+            if (y == stacks[z].length - 1) {
                 stackLastNote.innerHTML += `
                 <div id="${stacks[z][y]}" class="card" draggable="true" ondragstart="drag(event)">
                     <img src="./media/cards/${stacks[z][y]}.png" draggable="false"   alt="">
                 </div>
                 `;
+
+                if(!discovered.includes(stacks[z][y])) {
+                    discovered.push(stacks[z][y]);
+                }
             } else {
-                stackLastNote.innerHTML += `
-                <div id="${stacks[z][y]}" class="card" draggable="true" ondragstart="drag(event)">
-                    <img src="./media/cards/backside2.png" draggable="false"   alt="">
-                </div>
-                `;
+                if(discovered.includes(stacks[z][y])) {
+                    stackLastNote.innerHTML += `
+                    <div id="${stacks[z][y]}" class="card" draggable="true" ondragstart="drag(event)">
+                        <img src="./media/cards/${stacks[z][y]}.png" draggable="false"   alt="">
+                    </div>
+                    `;
+                } else {
+                    stackLastNote.innerHTML += `
+                    <div id="${stacks[z][y]}" class="card" draggable="true" ondragstart="drag(event)">
+                        <img src="./media/cards/backside2.png" draggable="false"   alt="">
+                    </div>
+                    `;
+                }
             }
-            
+
             stackLastNote = document.getElementById(stacks[z][y]);
+        }
     }
-}
-
-
-
 
 }
 
